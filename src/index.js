7ce556cc8e2faf03978a6191d71b441afb8faffc
andimags <andimagsumbol@gmail.com>
2024-06-17 19:53:04 +0800

    used css variables

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
-var perPage = 0;
+document.addEventListener("DOMContentLoaded", function () {
+    // 1 slide on small screens
+    var perPage = document.body.clientWidth >= 768 ? 2 : 1;
 
-var onresize = function () {
-    let width = document.body.clientWidth;
+    var onresize = function () {
+        let width = document.body.clientWidth;
 
-    perPage = width >= 768 ? 2 : 1;
-    splide.options.perPage = perPage;
-    splide.refresh();
-};
+        perPage = width >= 768 ? 2 : 1;
+        splide.options.perPage = perPage;
+        splide.refresh();
+    };
 
-window.addEventListener("resize", onresize);
+    // Listen when resizing the screen
+    window.addEventListener("resize", onresize);
 
-var splide = new Splide(".splide", {
-    perPage: 2,
-    rewind: true,
-    gap: "1rem",
-    padding: "2rem",
+    var splide = new Splide(".splide", {
+        perPage: perPage,
+        rewind: true,
+        gap: "1rem",
+        padding: "2rem",
+        marginRight: "10rem",
+    });
+
+    splide.mount();
 });
-splide.mount();
